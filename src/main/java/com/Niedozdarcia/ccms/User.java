0826562c933f8c0138600345b922c3cebb00f154
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-11 10:14:10 +0200

    Implement abstract class user.

---
+++ src/main/java/com/Niedozdarcia/ccms/User.java
@@ -0,0 +1,41 @@
+package com.Niedozdarcia.ccms;
+
+public abstract class User{
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public abstract void activate(View view);
+}
