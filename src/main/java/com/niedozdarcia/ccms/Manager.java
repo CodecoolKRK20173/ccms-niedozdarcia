87b4281df8f749c7a20d50ded7512ab3743211ba
Maziarella <kiebala.magdalena@gmail.com>
2018-07-11 15:54:09 +0200

    In class Manager Override method activate(), and implement private methods getDataFromUser(), editdData(). Code refactoring.

--- src/main/java/com/niedozdarcia/ccms/Manager.java
+++ src/main/java/com/niedozdarcia/ccms/Manager.java
@@ -30,43 +30,24 @@ public class Manager extends User {
     }
 
 
-    private void addMentor() {
+
+    private void getDataFromUser(){
         String emile = getView().getInput();
         String password = getView().getInput();
         String name = getView().getInput();
         String surname = getView().getInput();
+    }
 
+
+    private void addMentor() {
+        getDataFromUser();
         mentors.add(new Mentor(email, password, name, surname, students, assigments));
     }
 
 
-    private void removeMentor(int index) {
-        mentors.remove(index);
-    }
-
-
-    private void showMentors() {
-        getView().print(makeTable(mentors));
-    }
-
-
     private void addEmpoloyee() {
-        String emile = getView().getInput();
-        String password = getView().getInput();
-        String name = getView().getInput();
-        String surname = getView().getInput();
-
+        getDataFromUser();
         employees.add(new Employee(email, password, name, surname, students));
-    }
-
-
-    private void removeEmployee(int index) {
-        employees.remove(index);
-    }
-
-
-    private void showEmployees() {
-        getView().print(makeTable(employees));
     }
 
 
@@ -81,8 +62,158 @@ public class Manager extends User {
     }
 
 
+    private void showMentors() {
+        getView().print(makeTable(mentors));
+    }
+
+    private void showEmployees() {
+        getView().print(makeTable(employees));
+    }
+
+
+    private void showStudents() {
+        getView().print(makeTable(students));
+    }
+
+
+    private void removeMentor() {
+        showMentors();
+        int index = getView().getInputInt("Enter index of mentor who you want to remove.");
+        getView().getInputInt("Mentor %s removed", mentors.get(index).getName());
+        mentors.remove(index);
+
+    }
+
+    private void removeEmployee() {
+        showEmployees();
+        int index = getView().getInputInt("Enter index of employee who you want to remove.");
+        getView().getInputInt("Employee %s removed", mentors.get(index).getName());
+        employees.remove(index);
+    }
+
+
+    private void editData(List<User> users) {
+        int index = getView().getInputInt("Enter index of person who you want to edit.");
+
+        boolean isFinish = false;
+        while (!isFinish) {
+            getView().printMenu("Back to main menu",
+                                "Edit name",
+                                "Edit surname",
+                                "Edit password",
+                                "Edit email");
+
+            int choice = getView().getInputInt("Option: ");
+
+            switch (choice) {
+                case 0:
+                    isFinish = true;
+                    break;
+
+                case 1:
+                    String name = getView().getInputString("Enter new name: ");
+                    users.get(index).setName(name);
+                    break;
+
+                case 2:
+                    String surname = getView().getInputString("Enter new surname: ");
+                    users.get(index).setSurname(surname);
+                    break;
+
+                case 3:
+                    String password = getView().getInputString("Enter new password: ");
+                    users.get(index).setPassword(password);
+                    break;
+
+                case 4:
+                    String email = getView().getInputString("Enter new email: ");
+                    users.get(index).setEmail(email);
+                    break;
+
+                default:
+                    getView().print("There is not that option");
+            }
+        }
+    }
+
+
+    private void editMentorsData(){
+        showMentors();
+        editData(mentors);
+    }
+
+
+    private void editEmployeeData(){
+        showEmployees();
+        editData(employees);
+    }
+
+
     @Override
     public void activate(View view){
         setView(view);
+
+        boolean isFinish = false;
+        while (!isFinish) {
+            getView().printMenu("Exit",
+                                "Add mentor",
+                                "Remove mentor",
+                                "Edit mentor's data",
+                                "List of mentors",
+                                "Add employee",
+                                "Remove employee",
+                                "Edit employee's data",
+                                "List of employees",
+                                "List of students");
+
+            int choice = getView().getInputInt("Option: ");
+
+            switch (choice) {
+                case 0:
+                    isFinish = true;
+                    break;
+
+                case 1:
+                    addMentor();
+                    break;
+
+                case 2:
+                    removeMentor();
+                    break;
+
+                case 3:
+                    editMentorsData();
+                    break;
+
+                case 4:
+                    showMentors();
+                    break;
+
+                case 5:
+                    addEmpoloyee();
+                    break;
+
+                case 6:
+                    removeEmployee();
+                    break;
+
+                case 7:
+                    editEmployeeData();
+                    break;
+
+                case 8:
+                    showEmployees();
+                    break;
+
+                case 9:
+                    showStudents();
+                    break;
+
+
+                default:
+                    getView().print("There is not that option");
+            }
+        }
+
     }
 }
