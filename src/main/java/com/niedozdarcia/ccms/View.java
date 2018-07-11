06e758492026cadc6fb3ea5794b19f79c6bae38d
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-07-11 13:10:09 +0200

    Add View class

---
+++ src/main/java/com/niedozdarcia/ccms/View.java
@@ -0,0 +1,38 @@
+package com.niedozdarcia.ccms;
+
+import java.util.Scanner;
+
+public class View {
+
+    private Scanner scanner;
+
+    public View() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getInputString(String text) {
+        System.out.print(text);
+        return scanner.nextLine();
+    }
+
+    public int getInputInt(String text) {
+        System.out.print(text);
+        int input = 0;
+
+
+        while (input == 0) {
+
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid parameter");
+            }
+        }
+
+        return input;
+    }
+
+    public void print(String text) {
+        System.out.println(text);
+    }
+}
