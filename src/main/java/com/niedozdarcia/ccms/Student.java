c99d8809e2584efcb2a676a4164420252725a6dc
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-11 14:51:37 +0200

    Debugging

--- src/main/java/com/niedozdarcia/ccms/Student.java
+++ src/main/java/com/niedozdarcia/ccms/Student.java
@@ -1,11 +1,12 @@
 package com.niedozdarcia.ccms;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Student extends User{
-    private HashMap<String,String> assignments;
+    private Map<String,String> assignments;
 
-    public Student (String email, String password, String name, String surname, HashMap<String, String> assignments, View view){
+    public Student (String email, String password, String name, String surname, HashMap<String, String> assignments){
         setEmail(email);
         setPassword(password);
         setName(name);
@@ -14,7 +15,7 @@ public class Student extends User{
     }
 
     public void addAssignment(String assignment){
-        assignments.put(assignment.substring(0, assignment.length() - 1), assignment.substring(assignment.length() - 2, assignment.length() - 1));
+        assignments.put(assignment.substring(0, assignment.length() - 2), assignment.substring(assignment.length() - 2, assignment.length() - 1));
     }
 
     private void submitAssignment(String assignment){
@@ -24,21 +25,24 @@ public class Student extends User{
     }
 
     private void showAssigments(View view){
-        view.showAssigments(assignments);
+        for (String key : assignments.keySet()){
+            view.print(key + " " + assignments.get(key));
+        }
     }
 
     public void changeAssignmentStatus(String assignment, String mark){
-        assignments.get(assignment) = mark;
+        assignments.put(assignment, mark);
     }
 
     public void activate(View view){
+        setView(view);
         int input = 5;
         while(!(input == 0)){
-            view.printStudentMenu();
-            input = view.getInput();
+            view.printMenu("Exit", "SubmitAssignment", "ShowAssignment");
+            input = view.getInputInt("Pick option");
             if (input == 1){
                 showAssigments(view);
-                String assignment = view.getString("Assignment name?");
+                String assignment = view.getInputString("Assignment name?");
                 submitAssignment(assignment);
             }
             if (input == 2){
