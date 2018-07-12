cafbabc5e67b0a4fcf262a950d771d114337ccf4
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-12 13:44:12 +0200

    Created MentorsCSVDao

--- src/main/java/com/niedozdarcia/ccms/MentorsCSVDAO.java
+++ src/main/java/com/niedozdarcia/ccms/MentorsCSVDAO.java
@@ -10,7 +10,7 @@ public class MentorsCSVDAO extends  CSVHandler {
     private StudentCSVDAO studentsDao = new StudentCSVDAO();
     private AssignmentCSVDAO assignmentDAO = new AssignmentCSVDAO();
 
-    public MentorsCSVDAO(){
+    public MentorsCSVDAO() {
         setFilePath(HandleCsv.class.getResource("/users/mentors.csv").getPath());
         students = studentsDao.getStudents();
         assignments = assignmentDAO.getAssignments();
@@ -18,26 +18,22 @@ public class MentorsCSVDAO extends  CSVHandler {
         prepareData();
     }
 
-    public void prepareData(){
+    public void prepareData() {
         mentors = new ArrayList<>();
-        for (String[] record : getRecords()){
+        for (String[] record : getRecords()) {
             mentors.add(new Mentor(record[0], record[1], record[2], record[3], students, assignments));
         }
     }
 
-    public void prepareDataToSave(){
+    public void prepareDataToSave() {
 
-        for (Mentor mentor : mentors){
-            String[] preparedString = new String[4 + Student.getAssignments().size()];
-            preparedString[0] = student.getEmail();
-            preparedString[1] = student.getPassword();
-            preparedString[2] = student.getName();
-            preparedString[3] = student.getSurname();
-            int i = 4;
-            for (String assignment : Student.getAssignments()){
-                preparedString[i] = assignment;
-                i++;
-            }
+        for (Mentor mentor : mentors) {
+            String[] preparedString = new String[4];
+            preparedString[0] = mentor.getEmail();
+            preparedString[1] = mentor.getPassword();
+            preparedString[2] = mentor.getName();
+            preparedString[3] = mentor.getSurname();
             addRecordToSave(preparedString);
         }
+    }
 }
