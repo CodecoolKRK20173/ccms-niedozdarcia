49670aaca36a7aa1d85cc90072aa71e236a78257
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-07-12 14:00:36 +0200

    Implemented EmployessDAO

--- src/main/java/com/niedozdarcia/ccms/AttendanceCSVDao.java
+++ src/main/java/com/niedozdarcia/ccms/AttendanceCSVDao.java
@@ -2,7 +2,6 @@ package com.niedozdarcia.ccms;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AttendanceCSVDao extends CSVHandler {
---
+++ src/main/java/com/niedozdarcia/ccms/EmployessCSVDAO.java
@@ -0,0 +1,39 @@
+package com.niedozdarcia.ccms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployessCSVDAO extends  CSVHandler{
+    private List<Employee> employees;
+    private List<Student> students;
+    private StudentCSVDAO studentsDao = new StudentCSVDAO();
+
+    public EmployessCSVDAO() {
+        setFilePath(HandleCsv.class.getResource("/users/mentors.csv").getPath());
+        students = studentsDao.getStudents();
+        load();
+        prepareData();
+    }
+
+    public void prepareData() {
+        employees = new ArrayList<>();
+        for (String[] record : getRecords()) {
+            employees.add(new Employee(record[0], record[1], record[2], record[3], students));
+        }
+    }
+
+    public void prepareDataToSave() {
+        for (Employee employee : employees) {
+            String[] preparedString = new String[4];
+            preparedString[0] = employee.getEmail();
+            preparedString[1] = employee.getPassword();
+            preparedString[2] = employee.getName();
+            preparedString[3] = employee.getSurname();
+            addRecordToSave(preparedString);
+        }
+    }
+
+    public List<Employee> getMentors() {
+        return employees;
+    }
+}
